b956bfae420e5c9bdd4f0964ae81248dfb0fb695
Anisa Muziya Rafa <amrxll08@gmail.com>
2024-11-12 12:55:05 +0800

    Create f(x).py

---
+++ f(x).py
@@ -0,0 +1,8 @@
+# f(x) = 5x - 10
+def fungsi (x) :
+    return (5*x) - 10 
+a = int(input("masuukkan nilai x: "))       
+
+# hasil fx jika nilai x=10
+hasil_fx = fungsi (a)
+print("hasil fx adalah ", hasil_fx)
