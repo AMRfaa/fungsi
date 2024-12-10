7e2b484b9efa677bb1b124c70751e7faae94a054
Anisa Muziya Rafa <amrxll08@gmail.com>
2024-12-10 12:55:37 +0800

    Create luas_lingkaran.py

---
+++ luas_lingkaran.py
@@ -0,0 +1,15 @@
+# 1.(C) fungsi luas lingkaran 
+def luaslingkaran(jarijari):
+   return 3.14*(jarijari**2)
+a = int(input("masukkan nilai jari-jari: "))
+
+hasil = luaslingkaran(a)
+print(hasil)
+
+# 1.(D) fungsi mencari f(x)
+# f(x) = 5x - 10 
+def fungsi(x):
+   return 5*x - 10
+a = int(input("masukkan nilai x: "))
+hasil = fungsi(a)
+print(hasil)
